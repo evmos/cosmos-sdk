7e4a59ae9057f7ac604ef355e4f4af35e82b1664
Austin Chandra <austinchandra@berkeley.edu>
2023-03-13 22:27:32 -0700

    Use existing DefaultGasAdjustment field

--- client/tx/tx.go
+++ client/tx/tx.go
@@ -74,7 +74,7 @@ func BroadcastTx(clientCtx client.Context, txf Factory, msgs ...sdk.Msg) error {
 			return err
 		}
 
-		txf = txf.WithGas(uint64(float64(adjusted) * 1.25))
+		txf = txf.WithGas(adjusted)
 		_, _ = fmt.Fprintf(os.Stderr, "%s\n", GasEstimateResponse{GasEstimate: txf.Gas()})
 	}
 
